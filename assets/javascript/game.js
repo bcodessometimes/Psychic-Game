2a5abc4d6808364dce9066495147b3ab337b2b40
Rebecca Potter <rebeccapotter@Rebeccas-MacBook-Air.local>
2019-02-28 21:20:28 -0700

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@ var guessedLetter = [];
 var guessesLeft = 8;
 var computerChoice = "";
 var availableChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var correctGuess = false;
+
 
 //The computer picks a random letter
 var random = Math.floor(Math.random() * availableChoices.length)
